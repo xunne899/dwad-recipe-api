083d634808e388e817dd03dd06b6aa45cd045347
kunxin-chor <chorkunxin@yahoo.com>
2021-08-27 15:07:42 +0000

    Added update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongodb = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const MongoClient = mongodb.MongoClient;
 const dotenv = require('dotenv');
 dotenv.config();
@@ -26,17 +27,38 @@ async function connect() {
 async function main() {
     let db = await connect();
 
-    app.get('/recipes', async (req,res)=>{
+    app.get('/recipes', async (req, res) => {
         let recipes = await db.collection('recipes').find().toArray();
         res.json(recipes)
     })
 
-    app.post('/recipes', async(req,res)=>{
+    app.get('/recipes/:recipeId', async (req, res) => {
+        let r = await db.collection('recipes').findOne({
+            _id: new ObjectId(req.params.recipeId)
+        });
+        res.json(r);
+    })
+
+    app.post('/recipes', async (req, res) => {
         let results = await db.collection('recipes').insertOne({
             title: req.body.title,
             ingredients: req.body.ingredients
         })
         res.json(results.ops);
+    })
+
+    app.patch('/recipes/:id', async (req, res) => {
+        let results = await db.collection('recipes').updateOne({
+            '_id': new ObjectId(req.params.id),
+        }, {
+            '$set': {
+                'title': req.body.title,
+                'ingredients': req.body.ingredients
+            }
+        })
+        res.json({
+            'status': true
+        })
     })
 }
 
@@ -45,6 +67,6 @@ main();
 
 // START SERVER
 // note: we set port to 8888 so it won't clash with React
-app.listen(8888, ()=>{
+app.listen(8888, () => {
     console.log("server has started")
 })
